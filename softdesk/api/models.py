2ef14779a8f7cffc53814106e0597feeee8dea36
dev@KC <dev@kitconseil.fr>
2022-05-12 11:59:53 +0200

    feat: create dedicated USers

--- softdesk/api/models.py
+++ softdesk/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Users(AbstractUser):
+
+    email = models.EmailField(verbose_name='email address', unique=True,)  
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return "{}".format(self.email)
+
+
+class Contributors(Users):
+    pass
