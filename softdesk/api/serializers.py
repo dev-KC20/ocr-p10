4f308beaa5d4fe69c9b07d1a2b446c76d8d8fa29
dev@KC <dev@kitconseil.fr>
2022-05-23 15:32:50 +0200

    chore: add serializers

--- softdesk/api/serializers.py
+++ softdesk/api/serializers.py
@@ -1,13 +1,44 @@
 from rest_framework.serializers import ModelSerializer
- 
+
 from .models import Project
- 
+
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
 class ProjectSerializer(ModelSerializer):
-
     class Meta:
         model = Project
-        fields = '__all__'
+        fields = ["author_users", "title", "description", "type", "author"]
+
+
+class IssueSerializer(ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = ["project",
+                  "author_user",
+                  "assignee_user",
+                  "title",
+                  "description",
+                  "tag",
+                  "priority",
+                  "status",
+                  "created_time", ]
+
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["issue",
+                  "author_user",
+                  "description",
+                  "created_time", ]
+
+
+class ContributorSerializer(ModelSerializer):
+    class Meta:
+        model = Contributor
+        fields = ["user",
+                  "project",
+                  "role",
+                  "permission", ]
