c476ead6248f615ccd737767872590c180986b63
dev@KC <dev@kitconseil.fr>
2022-05-24 21:41:27 +0200

    feat: grant access to authenticated only

--- softdesk/api/views.py
+++ softdesk/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import ProjectSerializer
 from .models import Project
@@ -8,4 +8,4 @@ from .models import Project
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
