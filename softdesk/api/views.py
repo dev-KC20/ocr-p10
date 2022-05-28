d385fa74bc8fc3fe5003378161860dd8e952a012
dev@KC <dev@kitconseil.fr>
2022-05-28 11:48:43 +0200

    feat: filter project to members only

--- softdesk/api/views.py
+++ softdesk/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import ProjectSerializer
-from .models import Project
+from .models import Project, Contributor
 from .permissions import ContributorReadCreateAuthorUpdateDelete
 
 
@@ -11,3 +11,8 @@ class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
     permission_classes = [ContributorReadCreateAuthorUpdateDelete]
 
+    def get_queryset(self):
+        # filter the shown project to members only
+        project_contributed_to = Contributor.objects.filter(user=self.request.user).values_list('project_id')
+        print('filter project to members only, user:', self.request.user, project_contributed_to)
+        return Project.objects.filter(id__in=project_contributed_to )
