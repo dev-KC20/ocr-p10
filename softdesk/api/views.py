4e4dadd1a010d7d8b30c5a2f0ad3ba0b755b0a88
dev@KC <dev@kitconseil.fr>
2022-05-31 04:21:42 +0200

    fix: validate before create an issue

--- softdesk/api/views.py
+++ softdesk/api/views.py
@@ -119,6 +119,43 @@ class IssueViewSet(ModelViewSet):
 
         return queryset
 
+
+    def perform_create(self, serializer, *args, **kwargs):
+        # body of target data to be created
+        create_data = self.request.data
+        print('create issue data: ', create_data)
+        target_project_id = create_data['project']
+        target_author_id = create_data['author_user']
+        target_assignee_id = create_data['assignee_user']
+        # current project in the url
+        project_id = self.kwargs.get('project_pk')
+        print('create issue url: ', self.kwargs, project_id, target_project_id)
+        # the logged user needs to be member of the project
+        if not Contributor.objects.filter(project_id=target_project_id, user_id=self.request.user).exists():
+            error_message = f"You, user {self.request.user} need to be member of project {target_project_id}, pls work with your projects."
+            raise ValidationError(error_message)
+
+        # owasp : do not temper with project in the data
+        if not(int(project_id) == int(target_project_id)):
+            print('nothing to do here')
+            error_message = f"you are not allowed to work with project {target_project_id}, pls select: {project_id}"
+            raise ValidationError(error_message)
+
+        target_assignee = User.objects.get(pk=target_assignee_id)
+        if not target_assignee:
+            error_message = f"user {target_assignee_id} has to signup before you can add him to project {target_project_id}."
+            raise ValidationError(error_message)
+
+        if not Contributor.objects.filter(project_id=target_project_id, user_id=target_assignee_id).exists():
+            error_message = f"user {target_assignee_id} needs to be member of project {target_project_id}, pls add him to project before."
+            raise ValidationError(error_message)
+
+        # serializer.save(user=target_user, project_id=target_project_id, role=Issue.MEMBER,
+        #                 permission=Issue.CREATE_READ)
+        super().perform_create(serializer, *args, **kwargs)
+        print(f"issue added to project {target_project_id}!")
+    
+    
     # def destroy(self, request, *args, **kwargs):
     #     print('retrieve kwargs:', self.kwargs)
     #     contributor_to_delete = get_object_or_404(self.get_queryset())
@@ -132,33 +169,3 @@ class IssueViewSet(ModelViewSet):
     #     contributor = get_object_or_404(self.get_queryset())
     #     serializer = ContributorSerializer(contributor)
     #     return Response(serializer.data)
-
-    # def perform_create(self, serializer, *args, **kwargs):
-    #     # body of target data to be created
-    #     create_data = self.request.data
-    #     print('create contributor data: ', create_data)
-    #     target_project_id = create_data['project']
-    #     target_user_id = create_data['user']
-    #     # current project in the url
-    #     project_id = self.kwargs.get('project_pk')
-    #     print('create contributor url: ', self.kwargs, project_id, target_project_id)
-    #     # owasp : do not temper with project in the data
-    #     if not(int(project_id) == int(target_project_id)):
-    #         print('nothing to do here')
-    #         error_message = f"you are not allowed to work with project {target_project_id}, pls select: {project_id}"
-    #         raise ValidationError(error_message)
-    #     # get the author from Project rather than from Contributor
-    #     if not Project.objects.filter(pk=target_project_id, author_user=self.request.user).exists():
-    #         error_message = f"you are not the owner of project {target_project_id}, pls select another."
-    #         raise ValidationError(error_message)
-    #     target_user = User.objects.get(pk=target_user_id)
-    #     if not target_user:
-    #         error_message = f"user {target_user_id} has to signup before you can add him to project {target_project_id}."
-    #         raise ValidationError(error_message)
-    #     if Contributor.objects.filter(project_id=target_project_id, user_id=target_user_id).exists():
-    #         error_message = f"user {target_user_id} is already member of project {target_project_id}, pls select another."
-    #         raise ValidationError(error_message)
-
-    #     serializer.save(user=target_user, project_id=target_project_id, role=Contributor.MEMBER,
-    #                     permission=Contributor.CREATE_READ)
-    #     print(f"user {target_user_id} added to project {target_project_id}!")
